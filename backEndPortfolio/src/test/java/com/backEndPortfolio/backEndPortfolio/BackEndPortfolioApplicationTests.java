30a967e6ed395a91138fa450c891234e01cf6283
David Git Local <david.soto.test@gmail.com>
2022-12-22 10:30:29 -0300

    backend

---
+++ backEndPortfolio/src/test/java/com/backEndPortfolio/backEndPortfolio/BackEndPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backEndPortfolio.backEndPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
