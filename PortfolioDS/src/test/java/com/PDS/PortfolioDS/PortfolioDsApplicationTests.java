543905a071bf3995d7746ec206504e1648dd5b2b
David Git Local <david.soto.test@gmail.com>
2022-12-26 15:25:02 -0300

    new project

---
+++ PortfolioDS/src/test/java/com/PDS/PortfolioDS/PortfolioDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PDS.PortfolioDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
