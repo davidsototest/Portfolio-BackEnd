c4db3cf4f0f59054cce73302b833a17b788b8b8a
David Git Local <david.soto.test@gmail.com>
2023-01-09 15:05:11 -0300

    actualizar controller

--- src/main/java/com/PDS/PDS/controllers/WordController.java
+++ src/main/java/com/PDS/PDS/controllers/WordController.java
@@ -12,9 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:9001")
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE})
 @RestController
 @RequestMapping("/word")
 public class WordController {
