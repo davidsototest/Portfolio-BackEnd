31d4308c854b2d6ed0d18a2f20f215093c18ecd6
David Git Local <david.soto.test@gmail.com>
2023-01-09 13:03:44 -0300

    Agragar cors en todo los endpoints

--- src/main/java/com/PDS/PDS/WebConfig.java
+++ src/main/java/com/PDS/PDS/WebConfig.java
@@ -1,5 +1,6 @@
 package com.PDS.PDS;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,14 +9,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                        registry.addMapping("/**/**")
+                                .allowedOrigins("http://localhost:9001")
+                                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                                .maxAge(3600);
+                }
 
-    @Override
+        };
+}
 
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**");
-
-    }
+//    @Override
+//
+//    public void addCorsMappings(CorsRegistry registry) {
+//
+//        registry.addMapping("/**");
+//
+//    }
 
    
 
