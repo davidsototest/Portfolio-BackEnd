38033d4b13b850e8923529df3c9a85cf79239b85
David Git Local <david.soto.test@gmail.com>
2023-01-09 13:58:17 -0300

    Agragar cors solo en word y quitar la confg general

--- src/main/java/com/PDS/PDS/WebConfig.java
+++ src/main/java/com/PDS/PDS/WebConfig.java
@@ -1,29 +1,15 @@
 package com.PDS.PDS;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@EnableWebMvc
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                        registry.addMapping("/**")
-                                .allowedOrigins("http://localhost:9001")
-                                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                                .maxAge(3600);
-                }
-
-        };
-}
-
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@EnableWebMvc
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
 //    @Override
 //
 //    public void addCorsMappings(CorsRegistry registry) {
@@ -31,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
 //        registry.addMapping("/**");
 //
 //    }
-
-   
-
-}
+//
+//   
+//
+//}
--- src/main/java/com/PDS/PDS/controllers/WordController.java
+++ src/main/java/com/PDS/PDS/controllers/WordController.java
@@ -5,6 +5,7 @@ import com.PDS.PDS.models.WordModel;
 import com.PDS.PDS.services.WordService;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:9001")
 @RestController
 @RequestMapping("/word")
 public class WordController {
