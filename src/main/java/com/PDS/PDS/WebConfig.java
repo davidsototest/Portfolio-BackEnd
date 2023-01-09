b20db89e93e94daa85a6a82b2699c819f0643ec9
David Git Local <david.soto.test@gmail.com>
2023-01-09 16:32:46 -0300

    actualizar cors

--- src/main/java/com/PDS/PDS/WebConfig.java
+++ src/main/java/com/PDS/PDS/WebConfig.java
@@ -1,23 +1,22 @@
-//package com.PDS.PDS;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@EnableWebMvc
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//
-//    public void addCorsMappings(CorsRegistry registry) {
-//
-//        registry.addMapping("/**");
-//
-//    }
-//
-//   
-//
-//}
+package com.PDS.PDS;
+ 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**");
+
+    }
+
+   
+
+}
--- src/main/java/com/PDS/PDS/controllers/WordController.java
+++ src/main/java/com/PDS/PDS/controllers/WordController.java
@@ -5,17 +5,17 @@ import com.PDS.PDS.models.WordModel;
 import com.PDS.PDS.services.WordService;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE})
+//@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE})
 @RestController
 @RequestMapping("/word")
 public class WordController {
