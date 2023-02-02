3cec188dfd0d70f64c0a905e443393dce519905a
David Git Local <david.soto.test@gmail.com>
2023-02-02 20:47:34 -0300

    modificar el metodo de eliminar

--- src/main/java/com/PDS/PDS/controllers/WordController.java
+++ src/main/java/com/PDS/PDS/controllers/WordController.java
@@ -5,17 +5,14 @@ import com.PDS.PDS.models.WordModel;
 import com.PDS.PDS.services.WordService;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE})
 @RestController
 @RequestMapping("/word")
 public class WordController {
@@ -38,13 +35,13 @@ public class WordController {
     
     //metodo para eliminar word pasando word/1 1=ID del word
     @DeleteMapping ( path = "/{id}")
-    public String eliminarPorId(@PathVariable("id") int id){
-        boolean ok = this.wordService.eliminarWord(id);
-        if (ok){
-            return "se elimino el Trabajo con id: " + id;
-        } else{
-            return "No pudo eliminar el Trabajo con id: " + id;
-        }
+    public void eliminarPorId(@PathVariable("id") int id){
+        this.wordService.eliminarWord(id);
+//        if (ok){
+//            return "se elimino el Trabajo con id: " + id;
+//        } else{
+//            return "No pudo eliminar el Trabajo con id: " + id;
+//        }
     }
     
 }
